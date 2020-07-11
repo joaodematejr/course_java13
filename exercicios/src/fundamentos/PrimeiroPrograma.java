b0611d7e415c4c545419333393a3ecba6d1fd010
joaodematejr <joaodematejr@gmail.com>
2020-07-10 23:59:56 -0300

    Variavel e Constatantes

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,32 @@
 package fundamentos;
 
+/**
+ * 
+ * @author joaodematejr
+ * @since 1.0
+ * @version 1.0
+ * @apiNote
+ * 
+ */
+
 public class PrimeiroPrograma {
 	
 	public static void main(String[] args) {
+		
+		//UMA SENTENÇA DE CÓDIGO TERMINA COM;
 		System.out.println("Primeiro Programa !!!");
+		//MAIS UM COMENTARIO
+		
+		
+		/*
+		 LINHA 1 
+		 LINHA 2
+		 LINHA 3
+		 LINHA 4
+		 */
+		
+		
+		
 	}
 
 }
