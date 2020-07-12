edbc199db43d67a4e96303adca2764affc09b8bd
joaodematejr <joaodematejr@gmail.com>
2020-07-11 22:29:45 -0300

    Detalhes sobre o print, printf

---
+++ exercicios/src/fundamentos/Console.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+public class Console {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("Bom");
+		System.out.print(" Dia! \n\n");
+		
+		System.out.println("Bom");
+		System.out.println("Dia");
+		
+		System.out.printf("Megasena: %d %d %d %d %d %d \n\n", 1, 2, 3, 4, 5, 6);
+		
+		System.out.printf("Salário: %.1f", 1234.5678);
+		
+		System.out.printf("Nome: %s%n", "João");
+	}
+
+}
---
+++ exercicios/src/fundamentos/Scannear.java
@@ -0,0 +1,25 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Scannear {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite o seu nome: ");
+		String nome = entrada.nextLine();
+			
+		System.out.print("Digite o seu sobrenome: ");
+		String sobrenome = entrada.nextLine();
+		
+		System.out.print("Digite a sua idade: ");
+		int idade = entrada.nextInt();
+		
+		System.out.printf("%s %s tem %d anos.", nome, sobrenome, idade);
+		
+		entrada.close();
+	}
+
+}
