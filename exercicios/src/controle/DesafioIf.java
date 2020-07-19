97a3e638fd9160994573bdb6510a5b904fb351fd
joaodematejr <joaodematejr@gmail.com>
2020-07-18 21:09:17 -0300

    Finalização desafio IF

---
+++ exercicios/src/controle/DesafioIf.java
@@ -0,0 +1,17 @@
+package controle;
+
+public class DesafioIf {
+	
+	public static void main(String[] args) {
+		
+		double nota = 1.3;
+		
+		//NÃO USAR ; EM ESTRUTURAS DE CONTROLE (TEM UM EXCEÇÃO)
+		if (nota >= 9.0); {
+			System.out.println("Quadro de Honra!");
+			System.out.println("Você é fera !!!");
+			
+		}
+	}
+
+}
