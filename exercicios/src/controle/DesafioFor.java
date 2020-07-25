aafb260a9c3141422fabecc39f4f490640c55583
joaodematejr <joaodematejr@gmail.com>
2020-07-24 21:59:39 -0300

    Desafio For

---
+++ exercicios/src/controle/DesafioFor.java
@@ -0,0 +1,21 @@
+package controle;
+
+public class DesafioFor {
+
+	public static void main(String[] args) {
+
+		String valor = "#";
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(valor);
+			valor += "#";
+		}
+
+		// DESAFIO
+		// RESTRIÇÃO NÃO PODE VALOR NUMERICO PRA CONTROLAR O LAÇO;
+
+		for (String v = "#"; !v.equals("######"); v += "#") {
+			System.out.println(v);
+		}
+	}
+
+}
