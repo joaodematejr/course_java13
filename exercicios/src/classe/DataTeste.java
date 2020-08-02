000c63f94bdb439ae1844215b41a5a4c1b125a9d
joaodematejr <joaodematejr@gmail.com>
2020-08-02 02:53:52 -0300

    Metodos

--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -9,15 +9,17 @@ public class DataTeste {
 		d1.mes = 11;
 		d1.ano = 2021;
 		
-		System.out.printf("%d/%d/%d", d1.dia, d1.mes, d1.ano);
+		String dataFormadata1 = d1.obterDataFormatada();
+		
+		System.out.printf(dataFormadata1);
 		System.out.printf("\n");
 		
 		var d2 = new Data();
 		d2.dia = 31;
 		d2.mes = 12;
 		d2.ano = 2020;
-		
-		System.out.printf("%d/%d/%d", d2.dia, d2.mes, d2.ano);
+		String dataFormadata2 = d2.obterDataFormatada();
+		System.out.printf(dataFormadata2);
 		
 	}
 
--- exercicios/src/classe/Produto.java
+++ exercicios/src/classe/Produto.java
@@ -5,5 +5,13 @@ public class Produto {
 	String nome;
 	double preco;
 	double desconto;
+	
+	double precoComDesconto(){
+		return preco * (1 - desconto);	
+	}
+	
+	double precoComDesconto(double descontoDoGerente){
+		return preco * (1 - desconto + descontoDoGerente);
+	}
 
 }
