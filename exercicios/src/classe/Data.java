f219fe2c572e45b685f4c5fc6f256eba4c69bab8
joaodematejr <joaodematejr@gmail.com>
2020-08-03 20:45:31 -0300

    Finalização desafio construtor

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -6,6 +6,18 @@ public class Data {
 	int mes;
 	int ano;
 	
+	Data(){
+		dia = 01;
+		ano = 01;
+		ano = 1970;
+	}
+	
+	Data(int diaInicial, int mesInicial, int anoInicial){
+		dia = diaInicial;
+		mes = mesInicial;
+		ano = anoInicial;
+	}
+	
 	String obterDataFormatada() {
 		return String.format("%d/%d/%d", dia, mes, ano);
 		
--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -5,19 +5,13 @@ public class DataTeste {
 	public static void main(String[] args) {
 		
 		Data d1 = new Data();
-		d1.dia = 7;
-		d1.mes = 11;
-		d1.ano = 2021;
-		
+
 		String dataFormadata1 = d1.obterDataFormatada();
 		
 		System.out.printf(dataFormadata1);
 		System.out.printf("\n");
 		
-		var d2 = new Data();
-		d2.dia = 31;
-		d2.mes = 12;
-		d2.ano = 2020;
+		var d2 = new Data(31, 12, 2020);
 		String dataFormadata2 = d2.obterDataFormatada();
 		System.out.printf(dataFormadata2);
 		
