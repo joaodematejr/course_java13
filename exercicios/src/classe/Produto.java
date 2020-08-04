9fda1da49920c6bae564014a2561c813e800cdb2
joaodematejr <joaodematejr@gmail.com>
2020-08-03 23:21:39 -0300

    Desafio Classe

--- exercicios/src/classe/Produto.java
+++ exercicios/src/classe/Produto.java
@@ -1,26 +1,25 @@
 package classe;
 
 public class Produto {
-	
+
 	String nome;
 	double preco;
-	double desconto;
-	
-	Produto(String nomeInicial, double precoInicial, double descontoInicial) {
+	static double desconto = 0.25;
+
+	Produto(String nomeInicial, double precoInicial) {
 		nome = nomeInicial;
 		preco = precoInicial;
-		desconto = descontoInicial;
 	}
-	
+
 	public Produto() {
-		
+
 	}
-	
-	double precoComDesconto(){
-		return preco * (1 - desconto);	
+
+	double precoComDesconto() {
+		return preco * (1 - desconto);
 	}
-	
-	double precoComDesconto(double descontoDoGerente){
+
+	double precoComDesconto(double descontoDoGerente) {
 		return preco * (1 - desconto + descontoDoGerente);
 	}
 
--- exercicios/src/classe/ProdutoTeste.java
+++ exercicios/src/classe/ProdutoTeste.java
@@ -3,12 +3,13 @@ package classe;
 public class ProdutoTeste {
 	
 	public static void main(String[] args) {
-		Produto p1 = new Produto("Notebook", 4356.89, 0.25);	
+		Produto p1 = new Produto("Notebook", 4356.89);	
 		
 		var p2 = new Produto();
 		p2.nome = "Caneta Azul";
 		p2.preco = 12.56;
-		p2.desconto = 0.29;
+		
+		Produto.desconto = 0.29;
 		
 		System.out.println(p2.nome);
 		System.out.println(p1.nome);
