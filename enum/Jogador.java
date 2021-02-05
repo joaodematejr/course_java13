ef3ee77c0759514e71d50c21a3b0dbc058342ddf
João Dematé Jr <joaodematejr@gmail.com>
2021-02-04 21:31:43 -0300

    Enum

---
+++ enum/Jogador.java
@@ -0,0 +1,28 @@
+package curso;
+
+public class Jogador {
+	
+	int x;
+	int y;
+	
+	boolean andar(Direcao direcao) {
+		switch (direcao) {
+		case NORTE:
+			y--;
+			break;
+		case LESTE:
+			x++;
+			break;
+		case SUL:
+			y++;
+			break;
+		case OESTE:
+			x--;
+			break;
+		default:
+			break;
+		}
+		return true;
+	}
+
+}
