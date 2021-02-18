03667ad2df3e305525a5254c4f20461950d8b35e
João Dematé Jr <joaodematejr@gmail.com>
2021-02-17 23:32:01 -0300

    Polimorfismo 1

---
+++ polimorfismo1/Jantar.java
@@ -0,0 +1,30 @@
+package curso.polimorfismo;
+
+public class Jantar {
+	
+	public static void main(String[] args) {
+		
+		Pessoa convidado = new Pessoa(99.65);
+		
+		Arroz ingrediente1 = new Arroz(0.200);
+		Feijao ingrediente2 = new Feijao(0.100);
+		
+		System.out.println(convidado.getPeso());
+		
+		convidado.comer(ingrediente1);
+		convidado.comer(ingrediente2);
+		
+		System.out.println(convidado.getPeso());
+		
+		Sorvete sobremesa = new Sorvete(0.4);
+		
+		convidado.comer(sobremesa);
+		System.out.println(convidado.getPeso());
+		
+		
+		
+		
+		
+	}
+
+}
