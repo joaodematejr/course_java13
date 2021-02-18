748796b2fb1ae808b705f42858b0687c197f5552
João Dematé Jr <joaodematejr@gmail.com>
2021-02-17 22:41:50 -0300

    Get e Set

---
+++ getSet/Pessoa.java
@@ -0,0 +1,47 @@
+package curso;
+
+public class Pessoa {
+
+	public String nome;
+	public String sobrenome;
+	private int idade;
+
+	public Pessoa(String nome, String sobrenome, int idade) {
+		setNome(nome);
+		setSobrenome(sobrenome);
+		setIdade(idade);
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int novaIdade) {
+		novaIdade = Math.abs(novaIdade);
+		if (novaIdade >= 0 && novaIdade <= 120) {
+			this.idade = novaIdade;
+		}
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [nome=" + nome + ", idade=" + idade + "]";
+	}
+
+}
---
+++ getSet/PessoaTeste.java
@@ -0,0 +1,13 @@
+package curso;
+
+public class PessoaTeste {
+
+	public static void main(String[] args) {
+
+		Pessoa p1 = new Pessoa("João", "Dematé", 21);
+		p1.setIdade(130);
+
+		System.out.println(p1);
+	}
+
+}
